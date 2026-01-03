7697a054ed0c26fa106af872b8f4b2a966a134a0
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-03 13:22:19 +0300

    Adding first component

---
+++ my-first-vue/src/components/MyFirst.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>My first <span>Vue.js</span> componnet!</div>
+</template>
+
+<style scoped>
+  div {
+    color: #35495f;
+    font-size: 1.6rem;
+  }
+
+  span {
+    color: #41b883;
+    font-weight: 700;
+  }
+</style>
