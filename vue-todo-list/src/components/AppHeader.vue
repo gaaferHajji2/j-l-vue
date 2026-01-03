686f3e8333dbdd5c049408a57f44f31b3146814c
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-03 13:38:18 +0300

    Adding todo static header component

---
+++ vue-todo-list/src/components/AppHeader.vue
@@ -0,0 +1,6 @@
+<template>
+    <header>
+        <h1><span class="icon" aria-hidden="true">✔</span> To do</h1>
+        <p>Building Real-world Applications with Vue.js 3</p>
+    </header>
+</template>
