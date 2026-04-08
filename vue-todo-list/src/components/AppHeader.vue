76d19e882829bd5337c0f2f6b1bd0df539498096
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-08 16:28:32 +0300

    Adding styles to app header

--- vue-todo-list/src/components/AppHeader.vue
+++ vue-todo-list/src/components/AppHeader.vue
@@ -4,3 +4,26 @@
         <p>Building Real-world Applications with Vue.js 3</p>
     </header>
 </template>
+
+<style scoped>
+header {
+    border-bottom: #333 1px solid;
+    background-color: #fff;
+}
+
+header::after {
+    content: "";
+    display: block;
+    height: 1px;
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+}
+
+h1 {
+    font-size: 2rem;
+}
+
+h1 .icon {
+    font-size: 1rem;
+    vertical-align: middle;
+}
+</style>
