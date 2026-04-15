50d070dd11a1e3a2217e98ac893d57ff709770a8
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-15 14:15:57 +0300

    Init geoLocation component

---
+++ vue-todo-list/weather-app/src/components/GetLocation.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+type Geolocation = {
+    latitude: number;
+    longitude: number;
+}
+
+const coords: Ref<Geolocation|undefined> = ref()
+</script>
