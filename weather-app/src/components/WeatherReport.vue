c2cb2ca557b2f460bd7628b6d7ed311aafd3aa1e
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-16 13:39:06 +0300

    Init Weather report component

---
+++ weather-app/src/components/WeatherReport.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+type WeatherData = {
+    location: {
+        localtime: Date;
+        name: string;
+        region: string;
+    };
+    current: {
+        temp_c: number;
+        temp_f: number;
+        precip_mm: number;
+        condition: {
+            text: string;
+            icon: string;
+        };
+        wind_degree: number;
+        wind_kph: number;
+        wind_mph: number;
+    };
+}
+
+type Coords = { latitude: number; longitude: number }
+interface Props {
+  coords: Coords;
+}
+
+const props = defineProps<Props>()
+const data: Ref<WeatherData | undefined> = ref()
+</script>
