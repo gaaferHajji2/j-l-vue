f644291cab4788d309a91bda84c1936bd92b3fc8
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-16 13:07:54 +0300

    Define the geolocation handler

--- vue-todo-list/src/components/ListItem.vue
+++ vue-todo-list/src/components/ListItem.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    defineProps<{ 
+    defineProps<{
         isChecked?: boolean | false
     }>()
 </script>
--- weather-app/src/components/GetLocation.vue
+++ weather-app/src/components/GetLocation.vue
@@ -8,4 +8,13 @@ type Geolocation = {
 }
 
 const coords: Ref<Geolocation|undefined> = ref()
+const geolocationBlockedByUser: Ref<boolean> = ref(false)
+const getGeolocation = async (): Promise<void> => {
+    await navigator.geolocation.getCurrentPosition(
+        () => {}, 
+        (error: {message: string}) => {
+            geolocationBlockedByUser.value = true;
+            alert(`The error message is: ${error.message}`)
+        });
+}
 </script>
