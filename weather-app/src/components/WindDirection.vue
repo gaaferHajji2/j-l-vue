a52e91eb4967b08ed0cbee06f3d262cb86a64451
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-22 15:21:19 +0300

    Init wind direction component

---
+++ weather-app/src/components/WindDirection.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+interface Props {
+    degrees: number
+}
+
+const props = defineProps<Props>()
+
+const windStyle = computed(() => ({ "transform": "rotate("+props.degrees+"deg)"}))
+
+
+</script>
+
+<template>
+
+</template>
