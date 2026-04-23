0da521de202c17275163216c029464ceb1d7c1b3
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-23 13:16:46 +0300

    Remove unused whitespaces

--- weather-app/src/components/WindDirection.vue
+++ weather-app/src/components/WindDirection.vue
@@ -4,16 +4,13 @@ import { computed } from 'vue'
 interface Props {
     degrees: number
 }
-
 const props = defineProps<Props>()
-
 const windStyle = computed(() => ({ "transform": "rotate("+props.degrees+"deg)"}))
 </script>
 
 <template>
     <span class="inline-block" :style="windStyle" aria-hidden="true">
         <!-- sr for screen readers -->
-        ⬇️ <span class="sr-only">Wind Direction {{ degrees }} degress</span>
+        ⬇️ <span class="sr-only">Wind Direction {{ degrees }} degrees</span>
     </span>
-    
 </template>
