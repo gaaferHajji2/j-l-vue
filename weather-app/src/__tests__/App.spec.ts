cfcd2bf785203278f67842c990059fc35ba5a2f9
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-23 13:25:45 +0300

    Remove unused import & white spaces

--- weather-app/src/__tests__/App.spec.ts
+++ weather-app/src/__tests__/App.spec.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest'
 
-import { mount, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import App from '../App.vue'
 import GetLocation from '@/components/GetLocation.vue'
 
