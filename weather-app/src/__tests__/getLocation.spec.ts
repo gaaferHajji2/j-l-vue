377d3b86d5380a460e2b2451c66a5c4834878e96
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-29 13:19:45 +0300

    Adding GetLocation test with wrappers & async/await & mocking functions

---
+++ weather-app/src/__tests__/getLocation.spec.ts
@@ -0,0 +1,36 @@
+import GetLocation from "@/components/GetLocation.vue"
+import { shallowMount } from "@vue/test-utils"
+import { vi } from 'vitest'
+
+describe("GetLocation", () => {
+    // Here we need to change the test execution, because we retrieve async data.
+    it("should render without crashing", async (): Promise<void> => {
+        global.navigator.geolocation = {
+            getCurrentPosition: () => {},
+        }
+        const wrapper = await shallowMount(GetLocation)
+        expect(wrapper).toBeTruthy()
+    })
+
+    it("should return valid body with mocking", async (): Promise<void> => {
+        const mockGeoLocation = vi.fn((successCallback: Function) => {
+            const position = {
+                coords: {
+                    latitude: 51.5074,
+                    longitude: -0.1278,
+                },
+            };
+            successCallback(position)
+        })
+
+        global.navigator.geolocation = {
+            getCurrentPosition: mockGeoLocation,
+        }
+
+        const wrapper = shallowMount<GetLocation>(GetLocation)
+        expect(wrapper.vm.coords).toEqual({
+            latitude: 51.5074,
+            longitude: -0.1278,
+        })
+    })
+})
