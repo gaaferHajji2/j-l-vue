bbec23b825878a70c86b348f63f89e5c33939151
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-29 13:59:50 +0300

    Create success test with weather data & change shallowMount to mount

--- weather-app/src/__tests__/WeatherReport.spec.ts
+++ weather-app/src/__tests__/WeatherReport.spec.ts
@@ -1,5 +1,5 @@
 import WeatherReport from "@/components/WeatherReport.vue"
-import { shallowMount } from "@vue/test-utils"
+import { flushPromises, mount, shallowMount } from "@vue/test-utils"
 import { vi } from "vitest"
 
 
@@ -29,5 +29,55 @@ describe("Weather Report Tests", () => {
             }
         })
         expect(wrapper.html()).toContain("Loading...")
+    })
+
+    it("should return data", async () => {
+        const mockData = {
+            location: {
+                localtime: new Date(),
+                name: 'Eindhoven',
+                region: 'North Brabant',
+                country: 'Netherlands',
+                lat: 52.37,
+                lon: 4.9
+            },
+            current: {
+                temp_c: 10,
+                temp_f: 50,
+                humidity: 50,
+                precip_in: 0.1,
+                precip_mm: 2.5,
+                condition: {
+                text: 'Sunny',
+                icon: 'https://via.placeholder.com/150'
+                },
+                wind_degree: 180,
+                wind_dir: 'S',
+                wind_kph: 10,
+                wind_mph: 6.2
+            }
+        }
+
+        global.fetch = vi.fn(() => Promise.resolve({
+            json: () => Promise.resolve(mockData)
+        })) as any
+
+        const wrapper = await mount(WeatherReport, {
+            props: {
+                coords: {
+                    latitude: 0,
+                    longitude: 0
+                }
+            }
+        })
+
+        await flushPromises();
+
+        expect(wrapper.text()).toContain(mockData.current.condition.text)
+        expect(wrapper.text()).toContain(mockData.current.temp_c)
+        expect(wrapper.text()).toContain(mockData.location.name)
+        expect(wrapper.text()).toContain(mockData.location.region)
+        expect(wrapper.text()).toContain(mockData.current.wind_kph)
+        // expect(wrapper.text()).toContain(mockData.current.wind_degree)
     })
 })
