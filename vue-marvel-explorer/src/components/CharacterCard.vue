4cc1f9d2d4a205095cc421f3845115e073ba3293
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-13 15:46:54 +0300

    Init the character card

---
+++ vue-marvel-explorer/src/components/CharacterCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { Character } from '@/types/marvel'
+import CardView from './CardView.vue'
+
+interface Props {
+    character: Character
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+    
+</template>
