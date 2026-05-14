44414d84cff6aa2078f7e72c519536ee947ae21c
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-14 14:36:10 +0300

    Finish from search view

--- vue-marvel-explorer/src/views/SearchView.vue
+++ vue-marvel-explorer/src/views/SearchView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
+import CharacterOverview from '@/components/CharacterOverview.vue';
+
 </script>
 <template>
-  <div class="search">
-    <h1>This is a search page</h1>
-  </div>
+    <CharacterOverview />
 </template>
