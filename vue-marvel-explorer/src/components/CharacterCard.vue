ca3930958e24e49b076a6fee10db1441883224be
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-14 14:35:25 +0300

    Finish from character card

--- vue-marvel-explorer/src/components/CharacterCard.vue
+++ vue-marvel-explorer/src/components/CharacterCard.vue
@@ -10,5 +10,10 @@ const props = defineProps<Props>()
 </script>
 
 <template>
-    
+    <CardView :id="character.id">
+        <img :src="`${character.thumbnail.path}.${character.thumbnail.extension}`" 
+        class="float-left w-12 h-12 mb-2 mr-4 rounded-full shadow-md aspect-square" />
+        <template #header>{{ character.name }}</template>
+        <div class="text-base max-w-prose">{{ character.description }}</div>
+    </CardView>
 </template>
