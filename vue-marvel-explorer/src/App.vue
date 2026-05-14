e2bceaacb132923de754502c3f98b4812c6d8fe7
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-14 14:37:01 +0300

    Change the title of the page & adding styles

--- vue-marvel-explorer/src/App.vue
+++ vue-marvel-explorer/src/App.vue
@@ -3,7 +3,8 @@
 <template>
   <header>
     <div class="my-4 text-center">
-      <h1 class="mb-4 text-6xl font-extrabold uppercase">Marvel Explorer 🔭</h1>
+      <h1 class="mb-4 text-3xl font-extrabold uppercase bg-linear-to-r from-green-500/20 to-green-600
+      bg-clip-text text-transparent">Image Explorer With Dummy Comic And Character Data</h1>
       <nav>
         <RouterLink to="/" class="px-4 py-2 border-2 rounded-s-md hover:text-slate-600">📒 Comics</RouterLink>
         <RouterLink to="/search" class="px-4 py-2 border-2 border-s-0 hover:text-slate-600 rounded-e-md">🦹 Heroes</RouterLink>
