56dce6141a5ff49172f2f9f0bc56a6db2ee8e1eb
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-06-04 12:47:13 +0300

    Remove unused whitespaces

--- quiz-project/src/App.vue
+++ quiz-project/src/App.vue
@@ -1,36 +1,26 @@
 <script setup>
 
   import q from './data/quizes.json';
-
   import { ref, watch } from 'vue';
-
   import Card from './components/Card.vue'
-
   const quizes = ref(q);
-
   const search = ref("");
 
   watch(search, () => {
     console.log("Hello From Watch");
-
     quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
-
   });
-
 </script>
 
 <template>
-   
   <div class="container">
     <header>
       <h1>Quizzes</h1>
       <input v-model.trim="search" type="text" placeholder="Search...">
     </header>
-
     <div class="options-container">
       <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
     </div>
-
   </div>
 </template>
 
@@ -43,9 +33,7 @@
 header {
   margin-bottom: 10px;
   margin-top: 30px;
-
   display: flex;
-
   align-items: center;
 }
 
@@ -57,9 +45,7 @@ header h1 {
 header input {
   border: none;
   background-color: rgba(128, 128, 128, 0.1);
-
   padding: 10px;
-
   border-radius: 5px;
 }
 
