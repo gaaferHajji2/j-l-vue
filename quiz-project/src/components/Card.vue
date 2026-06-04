86261991ac8ec100be28a5c7a747ced975ac52b2
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-06-04 12:44:29 +0300

    Remove unused whitespaces & debug statement

--- quiz-project/src/components/Card.vue
+++ quiz-project/src/components/Card.vue
@@ -1,44 +1,30 @@
 <script setup>
-
 // defineProps({
 //     name: String,
 //     img: String,
 //     qLength: Number,
 // })
-
 let { quiz } = defineProps(['quiz']);
-
-console.log("The Props Is: ", quiz);
-
 </script>
 
 <template>
-
     <div class="card">
         <img :src="quiz.img" alt="" />
-
         <div class="card-text">
             <h2>{{ quiz.name }}</h2>
             <p>{{ quiz.questions.length }} Questions</p>
         </div>
     </div>
-
 </template>
 
 <style scoped>
 .card {
     width: 310px;
-
     overflow: hidden;
-
     border-radius: 2%;
-
     box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-
     margin-bottom: 35px;
-
     margin-right: 20px;
-
     cursor: pointer;
 }
 
